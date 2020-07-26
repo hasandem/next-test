71fd91710af521490ce497d5798a79993a56133c
Hans-Arne Sandem <hasandem@gmail.com>
2020-07-26 20:50:29 +0200

    Created more text on landing page

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 export default function Home() {
   return <div>
     <h1>Welcome to Sailog!</h1>
+    <h2>This is the neew generation sailing log</h2>
     </div>
   
 }
