4380a6aec2b9e1594c3d93d0278f0872d88bcb5d
hasandem <hasandem@gmail.com>
2020-12-29 07:26:55 +0100

    Corrected misspelling

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function Home() {
   return <div>
     <h1>Welcome to Sailog!</h1>
-    <h2>This is the neew generation sailing log</h2>
+    <h2>This is the new generation sailing log</h2>
     </div>
   
 }
